268c60104d5f762f5dfdbcab7c2d7ce818fa5ae7
Bianca Longhi <longhisbianca@gmail.com>
2018-08-13 22:35:51 -0300

    criando classe usuario e interface para usar retrofit

---
+++ app/src/main/java/br/edu/iff/pooa20181/usarretrofit/Usuario.java
@@ -0,0 +1,8 @@
+package br.edu.iff.pooa20181.usarretrofit;
+
+public class Usuario {
+    int id;
+    String name;
+    String login;
+}
+
---
+++ app/src/main/java/br/edu/iff/pooa20181/usarretrofit/interfaceretrofit/InterfaceRetrofitGithub.java
@@ -0,0 +1,17 @@
+package br.edu.iff.pooa20181.usarretrofit.interfaceretrofit;
+
+import br.edu.iff.pooa20181.usarretrofit.Usuario;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface InterfaceRetrofitGithub {
+    @GET("/users/{usuario}")
+    Call<Usuario> getUsuario(@Path("usuario") String usuario);
+    public static final Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+}
