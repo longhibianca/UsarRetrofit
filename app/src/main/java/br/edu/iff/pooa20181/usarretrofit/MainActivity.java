568745424a260c4b4bfd8b8e683861030f01f484
Bianca Longhi <longhisbianca@gmail.com>
2018-08-13 23:08:23 -0300

    retornando lista de seguidores

--- app/src/main/java/br/edu/iff/pooa20181/usarretrofit/MainActivity.java
+++ app/src/main/java/br/edu/iff/pooa20181/usarretrofit/MainActivity.java
@@ -2,18 +2,24 @@ package br.edu.iff.pooa20181.usarretrofit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.List;
 
 import br.edu.iff.pooa20181.usarretrofit.interfaceretrofit.InterfaceRetrofitGithub;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btUsuario;
+    Button btSeguidores;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,5 +70,32 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
-    }
+
+        btSeguidores = findViewById(R.id.btSeguidores);
+
+        btSeguidores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InterfaceRetrofitGithub githubUser = InterfaceRetrofitGithub.retrofit.create(InterfaceRetrofitGithub.class);
+
+                Call<Usuario> call = githubUser.getUsuario("lglmoura");
+
+                call.enqueue(new Callback<List<Usuario>>() {
+                    @Override
+                    public void onResponse(Call<List<Usuario>> call,
+
+                                           Response<List<Usuario>> response) {
+                        //pega a lista de usuarios e itera por ela pra mostrar no log
+                        List<Usuario> lista = response.body();
+                        for (Usuario usuario : lista) {
+                            Log.d("MainActivity", usuario.login);
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<List<Usuario>> call, Throwable t) {
+
+                    }
+            }
+        });
+    });
 }
--- app/src/main/java/br/edu/iff/pooa20181/usarretrofit/interfaceretrofit/InterfaceRetrofitGithub.java
+++ app/src/main/java/br/edu/iff/pooa20181/usarretrofit/interfaceretrofit/InterfaceRetrofitGithub.java
@@ -1,5 +1,7 @@
 package br.edu.iff.pooa20181.usarretrofit.interfaceretrofit;
 
+import java.util.List;
+
 import br.edu.iff.pooa20181.usarretrofit.Usuario;
 import retrofit2.Call;
 import retrofit2.Retrofit;
@@ -8,8 +10,15 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface InterfaceRetrofitGithub {
+    //chamada para retornar o nome do usuario
     @GET("/users/{usuario}")
     Call<Usuario> getUsuario(@Path("usuario") String usuario);
+
+    //chamada para retornar a lista de seguidores do usuario
+    @GET("/users/{usuario}/followers")
+    Call<List<Usuario>> getSeguidores(@Path("usuario") String usuario);
+
+
     public static final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("https://api.github.com/")
             .addConverterFactory(GsonConverterFactory.create())
